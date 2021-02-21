9269ad86182edcc730b5a917fe751c3c7b588978
pwksn <pawelkopciara@interia.pl>
2021-02-21 19:01:50 +0100

    CourseProgress errors fixed

--- src/Courses/CourseDetails/CourseProgress/CourseProgress.js
+++ src/Courses/CourseDetails/CourseProgress/CourseProgress.js
@@ -59,7 +59,7 @@ const CourseProgress = ({ currentCourse, onCourseAssignmentsChange }) => {
                     {!progressFormMode && <ion-icon name="pencil-outline" style={{color: '#fff'}}></ion-icon>}
                 </button>
             </div>
-            {!progressFormMode && <div className="course-progress-info">
+            {!progressFormMode && currentCourse.pointsPossible && <div className="course-progress-info">
                 <div className="course-progress-bar">
                     <CircleProgress 
                         percentage={pointsPercentage}
@@ -81,6 +81,9 @@ const CourseProgress = ({ currentCourse, onCourseAssignmentsChange }) => {
                         <p>Projected grade</p>
                     </div>
                 </div>
+            </div>}
+            {!progressFormMode && !currentCourse.pointsPossible && <div className="course-progress-empty">
+                <h2>No progress yet</h2>
             </div>}
             {/* {progressFormMode && <div className="course-progress-form-box">
                 <form className="course-progress-form" onSubmit={handleSubmit}>
--- src/Courses/CourseDetails/CourseProgress/CourseProgressForm.js
+++ src/Courses/CourseDetails/CourseProgress/CourseProgressForm.js
@@ -3,9 +3,9 @@ const CourseProgressForm = ({ pointsEarned, pointsPossible, setPointsEarned, set
         <div className="course-progress-form-box">
             <form className="course-progress-form" onSubmit={handleSubmit}>
                 <div className="d-flex">
-                    <input type="number" value={pointsEarned} onChange={(e) => setPointsEarned(e.target.value)} style={{direction: 'rtl'}}/>
+                    <input type="number" value={pointsEarned ? pointsEarned : 0} onChange={(e) => setPointsEarned(e.target.value)} style={{direction: 'rtl'}}/>
                     <p>/</p>
-                    <input type="number" value={pointsPossible} onChange={(e) => setPointsPossible(e.target.value)}/>
+                    <input type="number" value={pointsPossible ? pointsPossible : 0} onChange={(e) => setPointsPossible(e.target.value)}/>
                 </div>
                 <button type="submit">Submit</button>
             </form>
